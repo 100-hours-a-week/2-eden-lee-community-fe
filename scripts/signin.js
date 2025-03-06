902ccaa8a9920bf018b681c80915077d82f7d8d1
Yunju07 <juile0827@naver.com>
2025-03-06 23:45:06 +0900

    feat(sigin): 프로필 이미지 업로드시, 미리보기

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,5 +1,24 @@
 
 // TODO
-// 1. 프로필 이미지 업로드 시, 적용된 모습 미리보기 보여주기
 // 2. 각 항목 helper text 동적으로 보여주기
 // 3. < 버튼과 로그인 하러가기 버튼 클릭 시 login.html로 이동    
+
+document.addEventListener("DOMContentLoaded", function () {
+  const uploadContainer = document.getElementById("uploadContainer");
+  const fileInput = document.getElementById("profileImage");
+
+  // 파일 선택 시 미리보기 업데이트
+  fileInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+
+    if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+			uploadContainer.style.backgroundImage = `url('${e.target.result}')`;
+			uploadContainer.style.backgroundSize = "cover";
+			uploadContainer.style.backgroundPosition = "center";
+    };
+    reader.readAsDataURL(file);
+    }
+});
+});
